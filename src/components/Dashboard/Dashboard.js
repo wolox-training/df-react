4875840402fc1ab77b7bf9f6d98b0ef7e20a88b3
damfinkel <dfinkelstein@wolox.com.ar>
2017-11-03 12:15:49 -0300

    dashboard header with user information

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { DashboardHeader } from './DashboardHeader';
+
+export class Dashboard extends React.Component {
+  state = {};
+  render() {
+    return <DashboardHeader />;
+  }
+}
