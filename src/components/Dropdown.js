516c738fe96ddbed395eb3f8a369557c125f1e2c
damfinkel <dfinkelstein@wolox.com.ar>
2017-11-08 15:14:05 -0300

    dropdown dismiss by touching outside

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -4,24 +4,56 @@ import PropTypes from 'prop-types';
 
 import './Dropdown.css';
 
-function Dropdown(props) {
-  return (
-    <ul className={`nav-bar-user-dropdown ${props.show ? 'show' : ''}`}>
-      {props.options.map(options => (
-        <Link key={`${options.title}`} className="nav-bar-user-dropdown-item" to={options.path}>
-          {options.title}
-        </Link>
-      ))}
-    </ul>
-  );
+class Dropdown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentWillMount = () => document.addEventListener('click', this.handleClick, false);
+
+  componentWillUnmount = () => document.removeEventListener('click', this.handleClick, false);
+
+  setContainerRef = node => {
+    this.setState({ containerRef: node });
+  };
+
+  handleClick = e => {
+    if (!this.props.show && this.state.containerRef.contains(e.target)) {
+      return;
+    }
+    this.props.shouldDismiss(e);
+  };
+
+  render() {
+    return (
+      <ul className={`nav-bar-user-dropdown ${this.props.show ? 'show' : ''}`} ref={this.setContainerRef}>
+        {this.props.options.map(option => (
+          <Link
+            key={`${option.id}`}
+            className="nav-bar-user-dropdown-item"
+            to={option.path}
+            onClick={() => (option.action ? option.action(option.onSuccess, option.onError) : {})}
+          >
+            {option.title}
+          </Link>
+        ))}
+      </ul>
+    );
+  }
 }
 
 Dropdown.propTypes = {
-  show: PropTypes.bool,
+  show: PropTypes.bool.isRequired,
+  shouldDismiss: PropTypes.func.isRequired,
   options: PropTypes.arrayOf(
     PropTypes.shape({
-      title: PropTypes.string,
-      path: PropTypes.string
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      path: PropTypes.string.isRequired,
+      action: PropTypes.func,
+      onSuccess: PropTypes.func,
+      onError: PropTypes.func
     })
   ).isRequired
 };
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,36 +1,79 @@
 import React from 'react';
-import './NavigationBar.css';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 import wbooksLogo from '../assets/wbooks_logo.svg';
 import notificationsSvg from '../assets/notifications.svg';
 import addBookSvg from '../assets/add_book.svg';
 
 import Dropdown from './Dropdown';
+import './NavigationBar.css';
+
+const axiosInstance = axios.create({
+  baseURL: 'https://wbooks-api-stage.herokuapp.com/api/v1/'
+});
+
+const logout = (onSuccess, onError) => {
+  localStorage.removeItem('token');
+
+  // The below request doesn't work, but I don't have the real url to logout
+  axiosInstance
+    .delete('/users/sessions')
+    .then(response => {
+      if (onSuccess) {
+        onSuccess(response);
+      }
+    })
+    .catch(error => {
+      if (onError) {
+        onError(error);
+      }
+    });
+};
 
 const NAVBAR_USER_OPTIONS = [
   {
+    id: 1,
     title: 'Perfil',
-    path: '/'
+    path: '/',
+    action: undefined,
+    onSuccess: undefined,
+    onError: undefined
   },
   {
+    id: 2,
     title: 'Cerrar sesión',
-    path: '/'
+    path: '/login',
+    action: logout,
+    onSuccess: undefined,
+    onError: undefined
   }
 ];
 
 class NavigationBar extends React.Component {
   state = { showUserOptions: false };
 
+  setDropdownButtonRef = node => {
+    this.setState({ dropdownButtonRef: node });
+  };
+
   changeUserDropdownState = () => {
     this.setState({ showUserOptions: !this.state.showUserOptions });
+  };
+
+  dismissDropdown = e => {
+    if (this.state.dropdownButtonRef.contains(e.target)) {
+      return;
+    }
+    this.setState({ showUserOptions: false });
   };
 
   render() {
     return (
       <div className="navigation-bar">
-        <div className="nav-bar-logo-container">
+        <Link className="nav-bar-logo-container" to="/">
           <img src={wbooksLogo} alt="WBooks" className="navigation-logo" />
-        </div>
+        </Link>
         <div className="nav-bar-buttons-container">
           <button
             src={notificationsSvg}
@@ -43,8 +86,13 @@ class NavigationBar extends React.Component {
               className="nav-avatar-dropdown-button"
               alt="Settings"
               onClick={this.changeUserDropdownState}
+              ref={this.setDropdownButtonRef}
             />
-            <Dropdown show={this.state.showUserOptions} options={NAVBAR_USER_OPTIONS} />
+            <Dropdown
+              show={this.state.showUserOptions}
+              options={NAVBAR_USER_OPTIONS}
+              shouldDismiss={this.dismissDropdown}
+            />
           </div>
         </div>
       </div>
