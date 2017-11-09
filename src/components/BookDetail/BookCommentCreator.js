ccb96d72912b81e2fd5e0b54127781c142d093d3
damfinkel <dfinkelstein@wolox.com.ar>
2017-11-09 15:40:54 -0300

    added redux and store for comments only

--- src/components/BookDetail/BookCommentCreator.js
+++ src/components/BookDetail/BookCommentCreator.js
@@ -4,27 +4,34 @@ import PropTypes from 'prop-types';
 import avatarWbooksPng from '../../assets/avatar-wbooks.png';
 import './BookCommentCreator.css';
 
-function BookCommentCreator(props) {
-  return (
-    <div className="book-comment-creator-container">
-      <img className="book-comment-creator-avatar" src={avatarWbooksPng} alt="Avatar" />
-      <div className="book-comment-creator-form-container">
-        <span className="book-comment-creator-title">Agregar comentario</span>
-        <textarea
-          className="book-comment-creator-text"
-          onChange={e => props.newCommentChanged(e.target.value)}
-        />
-        <button className="book-comment-creator-button" onClick={props.newCommentHandler}>
-          Enviar
-        </button>
+class BookCommentCreator extends React.Component {
+  state = { currentComment: '' };
+
+  onCreateComment = () => {
+    this.props.createComment(this.state.currentComment);
+  };
+
+  render() {
+    return (
+      <div className="book-comment-creator-container">
+        <img className="book-comment-creator-avatar" src={avatarWbooksPng} alt="Avatar" />
+        <div className="book-comment-creator-form-container">
+          <span className="book-comment-creator-title">Agregar comentario</span>
+          <textarea
+            className="book-comment-creator-text"
+            onChange={e => this.setState({ currentComment: e.target.value })}
+          />
+          <button className="book-comment-creator-button" onClick={this.onCreateComment}>
+            Enviar
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 BookCommentCreator.propTypes = {
-  newCommentHandler: PropTypes.func.isRequired,
-  newCommentChanged: PropTypes.func.isRequired
+  createComment: PropTypes.func.isRequired
 };
 
 export default BookCommentCreator;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
+import store from './redux/Store';
 import Dashboard from './Dashboard/Dashboard';
 import BookDetail from './BookDetail/BookDetail';
 import Login from './Login/Login';
@@ -8,13 +10,15 @@ import './Main.css';
 
 function Main() {
   return (
-    <main className="main">
-      <Switch>
-        <PrivateRoute exact path="/" component={Dashboard} />
-        <PrivateRoute exact path="/books/:id" component={BookDetail} />
-        <AuthRoute path="/login" component={Login} />
-      </Switch>
-    </main>
+    <Provider store={store}>
+      <main className="main">
+        <Switch>
+          <PrivateRoute exact path="/" component={Dashboard} />
+          <PrivateRoute exact path="/books/:id" component={BookDetail} />
+          <AuthRoute path="/login" component={Login} />
+        </Switch>
+      </main>
+    </Provider>
   );
 }
 
